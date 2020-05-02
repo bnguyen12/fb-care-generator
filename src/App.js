18cfe32a88e101d696bd9a96feb573dad21537fc
Benny Nguyen <benny.nguyenx@gmail.com>
2020-05-02 14:50:50 -0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menubar from './components/Menubar'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Menubar />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import 'argon-design-system-react/src/assets/vendor/nucleo/css/nucleo.css'
 import 'argon-design-system-react/src/assets/vendor/font-awesome/css/font-awesome.min.css'
 import 'argon-design-system-react/src/assets/css/argon-design-system-react.css'
+import './index.css';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
