aa399a0b4aad910d3263aa890a334a3ebc0ff5fc
Allison <allison.lee124@gmail.com>
2020-05-15 20:01:50 -0700

    Create react hook to handle input text

--- src/components/UploadBar.js
+++ src/components/UploadBar.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import Spinner from 'react-bootstrap/Spinner';
-import { Button, Form, FormGroup, Input} from 'reactstrap';
+import React, {useState} from 'react';
+import { Button, Form, FormGroup, Input, Spinner, Container, Row, Col } from 'reactstrap';
 
 const UploadBar = (props) => {
+    const [image, setImage] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(image);
+    }
+
     return (
         <div>
-            <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-            </Spinner>
-            <Form inline>
-                <FormGroup>
-                    <Button>Submit</Button>
-                    <Input type="something" name="something" id="imageUpload" placeholder="Upload an image" />
-                </FormGroup>
-            </Form>
+            <Container>
+                <Row id="uploadBar">
+                    <Col xs="0.5">
+                        <Spinner color="primary" />
+                    </Col>
+                    <Col xs="auto">
+                        <Form inline onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Button>Submit</Button>
+                                <Input id="imageUpload" placeholder='Upload an image' value={image} onChange={e => setImage(e.target.value)}/>
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            {/* {
+                if (imageUploaded != '') {
+                    console.log("hi")
+                }
+            } */}
         </div>
     );
 }
