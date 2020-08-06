f246d838d23a55f62f59d63e181a4dbe93addf90
kenny <kennyn2@uw.edu>
2020-08-06 02:31:32 -0700

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Header from './components/Header';
+import { Jumbotron, Container } from 'reactstrap';
 import ResultCard from "./components/ResultCard";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Jumbotron fluid id="header">
+        <Container fluid>
+          <h1 className="display-5">Facebook Care Generator</h1>
+          <hr />
+          <p className="lead">Customize a care emoji to hold what you care about.</p>
+        </Container>
+      </Jumbotron>
       <div id="download-area">
         <ResultCard />
       </div>
